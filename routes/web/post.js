93eea7949cb8bf4874f94b55da8de97306a6a4d8
Ahamed Islam <ahmadzahir1750@gmail.com>
2022-08-20 08:56:00 -0300

    make it better

--- routes/web/post.js
+++ routes/web/post.js
@@ -53,7 +53,7 @@ router.post("/delete/:postId", async function(req, res){
 	Post.findOneAndRemove(req.params.postId).exec(function(err, posts){
         if(err){console.log(err);}
 
-        res.render("/posts/",{post:post});});});
+        res.redirect("/posts/");});});
 	
 
 
